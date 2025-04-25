4669f12d22bb96ed65484407f107c1781598b77a
johnhu <johnh@spinmaster.com>
2025-04-25 14:08:16 +0800

    create project

---
+++ GPCE500/A18_A34_DACOut/Project/Resource.h
@@ -0,0 +1,22 @@
+
+// Resource Table
+// Created by IDE, Do not modify this table
+
+extern unsigned long RES_Table;
+extern unsigned long SPI_Resources_Folder_Table;
+extern unsigned long RES_ANIMALS02_34K_SP4_SA;
+extern unsigned long RES_ANIMALS02_34K_SP4_EA;
+extern unsigned long RES_ANIMALS03_34K_SP4_SA;
+extern unsigned long RES_ANIMALS03_34K_SP4_EA;
+extern unsigned long RES_ANIMALS04_34K_SP4_SA;
+extern unsigned long RES_ANIMALS04_34K_SP4_EA;
+extern unsigned long RES_ANIMALS08_34K_SP4_SA;
+extern unsigned long RES_ANIMALS08_34K_SP4_EA;
+extern unsigned long RES_TAILOR3_16K_A18_SA;
+extern unsigned long RES_TAILOR3_16K_A18_EA;
+extern unsigned long RES_THEM2_SEEKCTRL_001_SP4_SA;
+extern unsigned long RES_THEM2_SEEKCTRL_001_SP4_EA;
+extern unsigned long RES_FILEMERGER_ROM_BIN_SA;
+extern unsigned long RES_FILEMERGER_ROM_BIN_EA;
+
+// End Table
